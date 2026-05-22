eac678d0c78b3d9b4274861d297a54972b40a473
atif2112011 <atif251171@gmail.com>
2026-05-22 09:23:24 +0530

    Added support for Vercel Server

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
 import { env } from "./env.js";
+let cachedConnection = null;
 
 export const connectDatabase = async () => {
   mongoose.set("strictQuery", true);
 
-  await mongoose.connect(env.mongodbUri);
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
+  const connection = await mongoose.connect(env.mongodbUri);
+  cachedConnection = connection;
   console.log("MongoDB connected");
+  return connection;
 };
