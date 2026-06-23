86a016d91f33f268497c9546f321abc6cb8cf3dd
atif2112011 <atif251171@gmail.com>
2026-06-24 01:01:41 +0530

    Lots of changes

--- backend/src/utils/apiResponse.js
+++ backend/src/utils/apiResponse.js
@@ -1,3 +1,5 @@
+import { env } from "../config/env";
+
 export const sendSuccess = (res, statusCode, message, data = null) => {
   if (env.nodeEnv == "production") {
     console.log("API Success", {res, statusCode, message, data:JSON.stringify(data)});
