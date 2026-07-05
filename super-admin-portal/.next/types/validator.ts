150efc66ed58fdc890d9bfb52f47350c548e36f6
atif2112011 <atif251171@gmail.com>
2026-07-05 11:28:02 +0530

    fixes

--- super-admin-portal/.next/types/validator.ts
+++ super-admin-portal/.next/types/validator.ts
@@ -236,6 +236,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/(dashboard)/risk-flags/[flagId]/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/risk-flags/[flagId]">> = Specific
+  const handler = {} as typeof import("../../src/app/(dashboard)/risk-flags/[flagId]/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/(dashboard)/risk-flags/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/risk-flags">> = Specific
